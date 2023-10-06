628d0e41952dccfea41269702eac5da2c4053c49
M Hadi Azarabad <mhazarabad@gmail.com>
2023-10-06 23:11:49 +0200

    bug fixed when switching between langs

---
+++ src/components/MenuContainerItalian.js
@@ -0,0 +1,73 @@
+
+import logo_image from '../statics/images/logo.png';
+import { useState } from 'react';
+import menu_it from './menu_it.json'
+import { Link } from 'react-router-dom';
+
+const MenuContainerEnglish = () => {
+    const [menuItems, setMenuItems] = useState(menu_it.menuItems);
+    const [menuCategory, setMenuCategory] = useState('');
+
+    const itemFilter = (category) => {
+        setMenuItems(menu_it.menuItems.filter(item => item.category === category));
+        setMenuCategory(category);
+    }
+
+    const slugifier = (name) => {
+        return name.replaceAll(' ', '_');
+    };
+
+    return (
+        <section className='food_section layout_padding'>
+            <ul className='langl' style={{ marginTop: "-30px", marginBottom: "60px" }}>
+                <li className='lang-links'><Link to={'/en'}>English</Link></li>
+                <li className='lang-links'><Link to={'/it'}>Italiano</Link></li>
+                <li className='lang-links active'><Link to={'/ar'}>العربیه</Link></li>
+            </ul>
+
+            <div className="container">
+
+                <div className="heading_container heading_center">
+                    <h2>{menu_it.ourmenu}</h2>
+                </div>
+
+                <ul className="filters_menu">
+                    {menu_it.menuCategory.map((category) => (
+                        <li className={menuCategory === category.name ? 'active' : 'not-active'} key={category.id} onClick={() => itemFilter(category.name)}>{category.name}</li>
+                    ))}
+                </ul>
+
+                <div className="filters-content">
+                    <div className="row grid">
+                        {menuItems.map((food) => (
+                            <div key={food.id} className={"col-sm-6 col-lg-4 all " + slugifier(food.category)}>
+                                <div className="box">
+                                    <div>
+                                        <div className="img-box">
+                                            <img src={food.image_src ? food.image_src : logo_image} alt="persianissiamo" />
+                                        </div>
+
+                                        <div className="detail-box">
+                                            <span style={{ float: "right" }}>{food.price + " €"}</span>
+                                            <h5>
+                                                {food.name}
+                                            </h5>
+                                            <p>
+                                                {food.description}
+                                            </p>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+    );
+}
+
+export default MenuContainerEnglish;
